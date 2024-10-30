a0643db765878c2650457bd7aa74f438c546a571
fitzy1321 <32967490+fitzy1321@users.noreply.github.com>
2024-10-29 22:53:12 -0600

    first

---
+++ exp-proj/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("OH SHIT, NO MORE CLASSES EVERYWHERE?");
+
+        void DoSomething()
+        {
+            Console.WriteLine("Does this work?");
+        }
+
+        DoSomething();
+
+        var age = 23;
+        var joe = new Person("Joe", age);
+
+        Console.WriteLine(joe);
+    }
+}
+
+public record Person(string Name, int Age);
