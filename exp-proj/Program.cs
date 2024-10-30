96407debb368a8ffeeca5349a91f183c32a02322
fitzy1321 <32967490+fitzy1321@users.noreply.github.com>
2024-10-29 23:13:08 -0600

    records are cool

--- exp-proj/Program.cs
+++ exp-proj/Program.cs
@@ -1,24 +1,16 @@
-﻿internal class Program
+﻿
+Console.WriteLine("Hello, C# World. It's been awhile");
+
+static void DoSomething()
 {
-
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-
-        Console.WriteLine("OH SHIT, NO MORE CLASSES EVERYWHERE?");
-
-        void DoSomething()
-        {
-            Console.WriteLine("Does this work?");
-        }
-
-        DoSomething();
-
-        var age = 23;
-        var joe = new Person("Joe", age);
-
-        Console.WriteLine(joe);
-    }
+    Console.WriteLine("Does this work?");
 }
+
+DoSomething();
+
+var age = 23;
+var joe = new Person("Joe", age);
+
+Console.WriteLine(joe);
 
 public record Person(string Name, int Age);
